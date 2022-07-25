9fe260f5f5bc069b05c92d0239742f558abed1e6
te25son <te25son@gmail.com>
2022-07-25 17:49:26 +0200

    Use pydantic models to better handle joke and error validation.

--- tests/jokes/test_joke.py
+++ tests/jokes/test_joke.py
@@ -1,8 +1,8 @@
 import pytest
 
 from typing import Any
-from jokes.joke import Joke
-from returns.maybe import Some, Nothing
+from jokes.models.joke import Joke
+from jokes.models.error import Error
 
 
 class TestJoke:
@@ -13,6 +13,7 @@ class TestJoke:
             'category': 'Dark',
             'type': 'single',
             'joke': "Some funny joke.",
+            'setup': "This shouldn't work.",
             'flags': {
                 'nsfw': False,
                 'religious': False,
@@ -55,54 +56,38 @@ class TestJoke:
 
 
     def test_single_joke_success(self, valid_single_data: dict[str, Any]):
-        joke = Joke.create(valid_single_data)
+        joke = Joke(**valid_single_data)
 
-        assert joke.type == Some(valid_single_data["type"])
-        assert joke.category == Some(valid_single_data["category"])
-        assert joke.joke == Some(valid_single_data["joke"])
-        assert joke.delivery == Nothing
-        assert joke.setup == Nothing
-        assert str(joke) == valid_single_data["joke"]
+        assert joke.type == valid_single_data["type"]
+        assert joke.category == valid_single_data["category"]
+        assert joke.joke == valid_single_data["joke"]
+        assert joke.delivery == None
+        assert joke.setup == None
+        assert joke.as_string() == valid_single_data["joke"]
 
 
     def test_twopart_joke_success(self, valid_twopart_data: dict[str, Any]):
-        joke = Joke.create(valid_twopart_data)
+        joke = Joke(**valid_twopart_data)
         setup = valid_twopart_data["setup"]
         delivery = valid_twopart_data["delivery"]
 
-        assert joke.type == Some(valid_twopart_data["type"])
-        assert joke.category == Some(valid_twopart_data["category"])
-        assert joke.joke == Nothing
-        assert joke.delivery == Some(valid_twopart_data["delivery"])
-        assert joke.setup == Some(valid_twopart_data["setup"])
-        assert str(joke) == "\n".join([setup, delivery])
+        assert joke.type == valid_twopart_data["type"]
+        assert joke.category == valid_twopart_data["category"]
+        assert joke.joke == None
+        assert joke.delivery == delivery
+        assert joke.setup == setup
+        assert joke.as_string() == "\n".join([setup, delivery])
 
 
     def test_error_success(self, valid_error_data: dict[str, Any]):
-        joke = Joke.create(valid_error_data)
+        error = Error(**valid_error_data)
 
-        assert joke.type == Nothing
-        assert joke.category == Nothing
-        assert joke.joke == Nothing
-        assert joke.delivery == Nothing
-        assert joke.setup == Nothing
-        assert joke.error.message == Some(valid_error_data["message"])
-        assert joke.error.additionalInfo == Some(valid_error_data["additionalInfo"])
-        assert joke.error.causedBy == Some(valid_error_data["causedBy"])
-        assert joke.error.internalError == valid_error_data["internalError"]
-        assert str(joke) == "\n".join(valid_error_data["causedBy"])
+        assert error.message == valid_error_data["message"]
+        assert error.additionalInfo == valid_error_data["additionalInfo"]
+        assert error.causedBy == valid_error_data["causedBy"]
+        assert error.internalError == valid_error_data["internalError"]
+        assert error.as_string() == "\n".join(valid_error_data["causedBy"])
 
 
     def test_joke_invalid_data(self):
-        joke = Joke.create({})
-
-        assert joke.type == Nothing
-        assert joke.category == Nothing
-        assert joke.joke == Nothing
-        assert joke.delivery == Nothing
-        assert joke.setup == Nothing
-        assert joke.error.internalError == False
-        assert joke.error.additionalInfo == Nothing
-        assert joke.error.causedBy == Nothing
-        assert joke.error.message == Nothing
-        assert str(joke) == "An unexpected error occurred."
+        pass
